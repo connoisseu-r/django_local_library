9d7a1b78435e0fc0ee9c4cabec170353b6c7a505
root <root@ip-172-31-5-203.ap-northeast-1.compute.internal>
2022-04-26 11:38:30 +0000

    First version of application moved into GitHub

--- meal_planner/meal_plans/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-
-urlpatterns = [
-        #Home page
-        path('', views.index, name='mealindex'),
-        ]
--- meal_planner/meal_plans/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def index(request):
-    """The home page for meal_plans."""
-    return render(request, 'meal_plans/mealindex.html')
